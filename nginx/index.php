e29584142dc63f7d623c929519dd2a9792077d2f
Dinar <Dinar_1994@live.com>
2023-01-25 13:16:18 +0400

    test add my page

---
+++ nginx/index.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+    <title>Hello World</title>
+    <body bgcolor="gray">
+        <Center>
+            <br>
+                <font color="red"><h2>Hello World</h2></font>
+            </br>
+        </Center>
+        <?php echo '<br>Local_IP:'.$_SERVER['SERVER_ADDR']; ?>
+    </body>
+</html>
