7453bdb20de642a545c9f37235201f04fd3d60f9
Dinar <Dinar_1994@live.com>
2023-01-25 13:37:19 +0400

    tmp

--- nginx/index.php
+++ nginx/index.php
@@ -1,12 +1,10 @@
-<html>
-<head>
-    <title>Hello World</title>
+<HTML>
+    <HEAD>
+        <title>Hello World</title>
     <body bgcolor="gray">
-        <Center>
-            <br>
-                <font color="red"><h2>Hello World</h2></font>
-            </br>
-        </Center>
-        <?php echo '<br>Local_IP:'.$_SERVER['SERVER_ADDR']; ?>
+    <Center>
+        <br><font color="red"><h2>Hello World</h2></br></font>
+    </Center>
+    <?php echo '<br>Local_IP:'.$_SERVER['SERVER_ADDR']; ?>
     </body>
-</html>
+</HTML>
